8d64644d55eb6c4d6ff58112b2b90df8f7835ff3
Prabhanjan <prabhanjan2906@gmail.com>
2025-09-14 02:16:06 -0500

    adding variables.tf

---
+++ infrastructure/variables.tf
@@ -0,0 +1,9 @@
+variable "NEWS_API_KEY" {
+  description = "API key for News API"
+  type        = string
+}
+
+variable "COUNTRY" {
+  description = "country to fetch news articles from"
+  type        = string
+}
