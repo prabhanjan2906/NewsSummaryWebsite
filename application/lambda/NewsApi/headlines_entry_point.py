4e4ac140200f2cfc2daf104ae23fcfe3ce066dd1
Prabhanjan <prabhanjan2906@gmail.com>
2025-09-14 09:47:55 -0500

    adding application code. lambda function to fetch news from newsapi

--- application/lambda/NewsApi/headlines_entry_point.py
+++ application/lambda/NewsApi/headlines_entry_point.py
@@ -6,5 +6,6 @@ import newsHeadlinesFetcher
 
 def handler(event, context):
     data = newsHeadlinesFetcher.getNews()
+    status_code = 200 if data and data['status'] == 'ok' else 417
     payload = data['articles'] if data else []
-    return {"statusCode": 200 if data else 417, "body": payload}
+    return {"statusCode": status_code, "body": payload}
