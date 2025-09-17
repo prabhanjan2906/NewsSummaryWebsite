117805c56d9651485eac7362fcaa1ee7f3ad0cbf
Prabhanjan <prabhanjan2906@gmail.com>
2025-09-16 20:07:44 -0500

    event scheduler

--- infrastructure/begin.tf
+++ infrastructure/begin.tf
@@ -4,3 +4,15 @@ module "lambda" {
   COUNTRY      = var.COUNTRY
   LANGUAGE     = var.LANGUAGE
 }
+
+output "lambda_function_name" {
+  value = module.lambda.lambda_function_name
+}
+
+output "lambda_datasource_arn" {
+  value = module.lambda.lambda_datasource_arn  
+}
+
+output "lambda_function_ds" {
+  value = module.lambda.headlines_lambda_function_name
+}
