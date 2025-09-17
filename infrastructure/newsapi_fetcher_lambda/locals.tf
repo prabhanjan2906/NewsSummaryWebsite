28ccc511b6a876c419a3631f079905b8bc42336d
Prabhanjan <prabhanjan2906@gmail.com>
2025-09-17 02:52:20 -0500

    Adding message queues and ensuring lambda gets new deployment of app each time

--- infrastructure/newsapi_fetcher_lambda/locals.tf
+++ infrastructure/newsapi_fetcher_lambda/locals.tf
@@ -2,4 +2,5 @@ locals {
     environment = "development"
     lambda_function_name = "news_headlines_function"
     lambda_schedule_rate = "rate(1 hour)"
+    news_fetcher_exec_role_name = "news-fetcher-role"
 }
