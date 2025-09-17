7a36244a229b543b8f0edb480108d6b438347835
Prabhanjan <prabhanjan2906@gmail.com>
2025-09-17 00:11:12 -0500

    event scheduler

--- infrastructure/begin.tf
+++ infrastructure/begin.tf
@@ -12,3 +12,10 @@ output "lambda_datasource_arn" {
 output "lambda_function_ds" {
   value = module.lambda.headlines_lambda_function_name
 }
+
+output "lambda_function_invalid" {
+  value = module.lambda.headlines_lambda_function_name_invalid
+}
+output "lambda_datasource_arn" {
+  value = module.lambda.lambda_function_arn_invalid
+}
--- infrastructure/newsapi_fetcher_lambda/eventscheduler.tf
+++ infrastructure/newsapi_fetcher_lambda/eventscheduler.tf
@@ -9,3 +9,13 @@ output "lambda_function_arn" {
 output "headlines_lambda_function_name" {
   value = data.aws_lambda_function.headlines 
 }
+
+data "aws_lambda_function" "headlinesinvalid" {
+  function_name = "invalid-function-name"
+}
+output "lambda_function_arn_invalid" {
+  value = data.aws_lambda_function.headlinesinvalid.arn
+}
+output "headlines_lambda_function_name_invalid" {
+  value = data.aws_lambda_function.headlinesinvalid
+}
