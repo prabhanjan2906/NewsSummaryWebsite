2c937b6fe2fbb44c0b00380689467c479c886736
Prabhanjan <prabhanjan2906@gmail.com>
2025-09-17 04:06:53 -0500

    ensuring lambda gets new deployment of app each time

--- application/lambda/NewsApi/headlines_entry_point.py
+++ application/lambda/NewsApi/headlines_entry_point.py
@@ -8,13 +8,12 @@ output_message_queue = os.getenv('NEWSAPI_OUTPUT_QUEUE_URL', None)
 
 def handler(event, context):
     data = newsHeadlinesFetcher.getNews()
-    payload = data['articles'] if data else []
     if output_message_queue and data:
         sqs_client = boto3.client('sqs')
 
-        for anObj in payload:
+        for anObj in data:
             sqs_client.send_message(
                 QueueUrl=output_message_queue,
                 MessageBody=json.dumps(anObj)
             )
-    return {"statusCode": 200 if data else 417, "body": len(payload)}
+    return {"statusCode": 200 if data else 417, "body": len(data) if data else 0}
