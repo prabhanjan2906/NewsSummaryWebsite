c3ac5f02806f6278c3f51e6e8fb32b7461fc6e72
Prabhanjan <prabhanjan2906@gmail.com>
2025-09-17 04:17:00 -0500

    ensuring lambda gets new deployment of app each time

--- infrastructure/newsapi_fetcher_lambda/lambda.tf
+++ infrastructure/newsapi_fetcher_lambda/lambda.tf
@@ -31,6 +31,7 @@ resource "aws_lambda_function" "news_headlines" {
   runtime       = "python3.9"
   role          = aws_iam_role.news_fetcher_exec.arn
   source_code_hash = filebase64sha256("${path.module}/lambda_news_api_fetcher.zip")
+  timeout       = 300
 
   environment {
     variables = {
