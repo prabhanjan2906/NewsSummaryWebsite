366dbe4ac949a9d6e4a844d5da0d51e7b660527c
Prabhanjan <prabhanjan2906@gmail.com>
2025-10-09 12:39:07 -0500

    cross region support

--- infrastructure/backend.hcl
+++ infrastructure/backend.hcl
@@ -1,5 +1,5 @@
 bucket         = "tfstatefiles3bucket-newssummarywebsite"
-key            = "newssummarywebsite/__REGION__/terraform.tfstate"
+key            = "newssummarywebsite/__ENV__/terraform.tfstate"
 region         = "__REGION__"
 dynamodb_table = "TerraformStateFileLockVariableTable"
 encrypt        = true
--- infrastructure_tf_state/main.tf
+++ infrastructure_tf_state/main.tf
@@ -1,6 +1,6 @@
 module "terraform" {
   source            = "./terraform_state_storage"
   REGION            = local.region
-  state_bucket_name = "${local.region}-tfstatefiles3bucket-newssummarywebsite"
+  state_bucket_name = "tfstatefiles3bucket-newssummarywebsite"
   lock_table_name   = "TerraformStateFileLockVariableTable"
 }
