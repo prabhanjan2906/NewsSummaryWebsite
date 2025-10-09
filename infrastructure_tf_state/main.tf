1324759242b60e8b3faefecbdd37a098736413e5
Prabhanjan <prabhanjan2906@gmail.com>
2025-10-09 05:07:18 -0500

    cross region support

--- infrastructure_tf_state/main.tf
+++ infrastructure_tf_state/main.tf
@@ -1,6 +1,6 @@
 module "terraform" {
   source            = "./terraform_state_storage"
-  region            = local.region
+  REGION            = local.region
   state_bucket_name = "tfstatefiles3bucket-newssummarywebsite"
   lock_table_name   = "TerraformStateFileLockVariableTable"
 }
