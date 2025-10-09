635434e2fb6d42fcdc565279ccf5c525473bebfd
Prabhanjan <prabhanjan2906@gmail.com>
2025-10-09 05:04:45 -0500

    cross region support

--- infrastructure_tf_state/terraform_state_storage/variables.tf
+++ infrastructure_tf_state/terraform_state_storage/variables.tf
@@ -1,3 +1,3 @@
-variable "region"            { type = string }
+variable "REGION"            { type = string }
 variable "state_bucket_name" { type = string }
 variable "lock_table_name"   { type = string }
