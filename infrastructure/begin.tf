c8f16b30a1e47a3e6850e9d144d94c716efb0b56
Prabhanjan <prabhanjan2906@gmail.com>
2025-11-25 23:24:11 -0600

    disabling

--- infrastructure/begin.tf
+++ infrastructure/begin.tf
@@ -6,7 +6,7 @@ module "lambda" {
 }
 
 module "consumer" {
-  source                    = "./consumer"
+  source = "./consumer"
   # newsapi_message_queue_arn = module.lambda.newsapi_message_queue_arn
   RAW_BUCKET                = var.RAW_BUCKET
   ENVIRONMENT               = var.ENVIRONMENT
