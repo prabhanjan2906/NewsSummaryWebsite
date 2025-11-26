abdff4cc2573a8736e69efe857debb5ead09439d
Prabhanjan <prabhanjan2906@gmail.com>
2025-11-25 23:44:52 -0600

    disabling

--- infrastructure/consumer_variables.tf
+++ infrastructure/consumer_variables.tf
@@ -1,14 +1,14 @@
-variable "RAW_BUCKET" {
-  description = "S3 bucket to store scraped raw webpage data"
-  type        = string
-}
+# variable "RAW_BUCKET" {
+#   description = "S3 bucket to store scraped raw webpage data"
+#   type        = string
+# }
 
-variable "ENVIRONMENT" {
-  description = "Target environment to run"
-  type        = string
-}
+# variable "ENVIRONMENT" {
+#   description = "Target environment to run"
+#   type        = string
+# }
 
-variable "RAW_BUCKET_INPUT_KEY" {
-  description = "prefix key to store the raw json data"
-  type        = string
-}
+# variable "RAW_BUCKET_INPUT_KEY" {
+#   description = "prefix key to store the raw json data"
+#   type        = string
+# }
--- infrastructure/trigger/trigger_variables.tf
+++ infrastructure/trigger/trigger_variables.tf
@@ -1,11 +1,11 @@
-variable "REGION" {
-  type = string
-}
+# variable "REGION" {
+#   type = string
+# }
 
-variable "RAW_BUCKET_OUTPUT_KEY" {
-  type = string
-}
+# variable "RAW_BUCKET_OUTPUT_KEY" {
+#   type = string
+# }
 
-variable "MODEL_ID" {
-  type = string
-}
+# variable "MODEL_ID" {
+#   type = string
+# }
