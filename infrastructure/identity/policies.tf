8e7bd63507f66ba1d6ba54b2ac5249069e990573
Prabhanjan <prabhanjan2906@gmail.com>
2025-12-02 23:47:51 -0600

    policy

--- infrastructure/identity/policies.tf
+++ infrastructure/identity/policies.tf
@@ -21,6 +21,7 @@ resource "aws_iam_policy" "github_actions_ec2_readonly" {
           "ec2:CreateVpc",
           "ec2:DescribeVpcAttribute",
           "ec2:DescribeAddressesAttribute",
+          "ec2:ModifyVpcAttribute",
           "ec2:*"
         ],
         Resource = "*"
