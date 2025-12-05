2c327ba39395075a9771838521472f0f70eff75d
Prabhanjan <prabhanjan2906@gmail.com>
2025-12-05 16:05:18 -0600

    db name

--- infrastructure/CleanerAndNormalizer/rds.tf
+++ infrastructure/CleanerAndNormalizer/rds.tf
@@ -33,6 +33,7 @@ resource "aws_db_instance" "newsdb" {
   engine            = "postgres"
   instance_class    = "db.t3.micro"
   identifier        = "${var.env}-news-db"
+  db_name = var.db_name
   username          = var.db_user
   password          = var.db_password
   allocated_storage = 20
